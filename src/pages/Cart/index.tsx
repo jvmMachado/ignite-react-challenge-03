485bfa091ec33457757b9d0f59e709e26265f9c2
jvmMachado <joaovmmachado@gmail.com>
2021-07-22 14:29:47 -0300

    add formated values and show cart total price

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -20,15 +20,15 @@ interface Product {
 const Cart = (): JSX.Element => {
   const { cart, removeProduct, updateProductAmount } = useCart();
 
-  // const cartFormatted = cart.map(product => ({
-  //   // TODO
-  // }))
-  // const total =
-  //   formatPrice(
-  //     cart.reduce((sumTotal, product) => {
-  //       // TODO
-  //     }, 0)
-  //   )
+  const cartFormatted = cart.map(product => ((
+   {...product, priceFormatted: formatPrice(product.price), subTotal: formatPrice(product.price * product.amount) }
+  )))
+  const total =
+    formatPrice(
+      cart.reduce((sumTotal, product) => {
+        return sumTotal += (product.amount * product.price);
+      }, 0)
+    )
 
   function handleProductIncrement(product: Product) {
     updateProductAmount({ productId: product.id, amount: (product.amount + 1) });
@@ -55,14 +55,14 @@ const Cart = (): JSX.Element => {
           </tr>
         </thead>
         <tbody>
-          {cart.map(product => (
+          {cartFormatted.map(product => (
             <tr key={product.id} data-testid="product">
             <td>
               <img src={product.image} alt={product.title} />
             </td>
             <td>
               <strong>{product.title}</strong>
-              <span>R$ {product.price}</span>
+              <span>{product.priceFormatted}</span>
             </td>
             <td>
               <div>
@@ -91,7 +91,7 @@ const Cart = (): JSX.Element => {
               </div>
             </td>
             <td>
-              <strong>R$ {product.amount * product.price}</strong>
+              <strong>{product.subTotal}</strong>
             </td>
             <td>
               <button
@@ -112,7 +112,7 @@ const Cart = (): JSX.Element => {
 
         <Total>
           <span>TOTAL</span>
-          <strong>R$ 359,80</strong>
+          <strong>{total}</strong>
         </Total>
       </footer>
     </Container>
